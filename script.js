fc7c9da911ddaac27fa0acc86d8899a41ac30723
PetrAhtimirov <75142573+PetrAhtimirov@users.noreply.github.com>
2020-12-09 23:03:44 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-let pageHeight = document.documentElement.clientHeight;
-let parallaxWindow = document.querySelector('.preview .parallax-window');
-parallaxWindow.style.height = pageHeight;
-console.log(pageHeight);
-
-let infoScroll = document.querySelector('.info-scroll');
-infoScroll.onclick = function () {
-  window.scrollTo(0, 10);
-};
